1e26036f6fcbe899b5e47f1ab89fd2c8d768d217
mra1412 <mrehanabbasi@yahoo.com>
2020-04-06 21:33:47 +0500

    Add .gitignore file to repo. Add simple sample code in server.js. Also remove some bugs in importing of express and joi. Replace joi-browser with joi.

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
-import express from 'express';
-import Joi from 'joi-browser';
+const express = require('express');
+const Joi = require('joi');
 const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen('3000', () => {
+  console.log(`Listening on port ${port}`);
+});
